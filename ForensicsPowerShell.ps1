b0ed548fe8a7ae89ce40bcdcac3d4c1f5ccd8e42
amonkeyslayer <rxs2165@rit.edu>
2017-09-07 15:43:14 -0700

    Start
    Still working on the info gathering. No idea how to actually get it into the csv file.

---
+++ ForensicsPowerShell.ps1
@@ -0,0 +1,11 @@
+﻿$csvData = New-Object -TypeName psobject
+$date = Get-Date
+$timezone = (Get-TimeZone).Id
+$uptime = $date - $operatingSystem.LastBootUpTime
+$computername = Get-CimInstance -ClassName Win32_ComputerSystem
+$os = Get-CimInstance -ClassName Win32_OperatingSystem
+$cpu = Get-CimInstance -ClassName CIM_Processor
+$ram = RAM -Value ("{0:N2}GB" -f ($computername.TotalPhysicalMemory/1GB))
+$disks = Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object DeviceID,VolumeName,FileSystem,@{l='Size';e={("{0:N2}GB" -f ($_.Size/1GB))}}
+
+$DC = Get-ADDomainController
